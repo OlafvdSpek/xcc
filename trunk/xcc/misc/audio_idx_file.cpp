a525b337dba84c4d407c616842688efc64d716a9
olafvdspek <olafvdspek@108d0302-aaf7-b7c6-26e7-3e45b3cf5337>
2000-11-26 20:44:45 +0000

    added audio_idx_file_write*

--- trunk/xcc/misc/audio_idx_file.cpp
+++ trunk/xcc/misc/audio_idx_file.cpp
@@ -3,5 +3,32 @@
 //////////////////////////////////////////////////////////////////////
 
 #include "stdafx.h"
+#include <assert.h>
 #include "audio_idx_file.h"
 
+int audio_idx_file_get_write_size(int c_sounds)
+{
+	return sizeof(t_audio_idx_header) + sizeof(t_audio_idx_entry) * c_sounds;
+}
+
+int audio_idx_file_write_header(void* w, int c_sounds)
+{
+	t_audio_idx_header& header = *reinterpret_cast<t_audio_idx_header*>(w);
+	header.id = audio_idx_id;
+	header.two = 2;
+	header.c_sounds = c_sounds;
+	return sizeof(t_audio_idx_header);
+}
+
+int audio_idx_file_write_entry(void* w, string fname, int offset, int size, int samplerate, int flags, int chunk_size)
+{
+	t_audio_idx_entry& entry = *reinterpret_cast<t_audio_idx_entry*>(w);
+	assert(fname.length() < 16);
+	strcpy(entry.fname, fname.c_str());
+	entry.offset = offset;
+	entry.size = size;
+	entry.samplerate = samplerate;
+	entry.flags = flags;
+	entry.chunk_size = chunk_size;
+	return sizeof(t_audio_idx_entry);
+}
