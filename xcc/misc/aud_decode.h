cc1114fd1ad1e8bc0bccc918350a96a7d40b948a
olafvdspek <olafvdspek@108d0302-aaf7-b7c6-26e7-3e45b3cf5337>
2000-11-26 20:43:45 +0000

    updated void init(int index = 0, int sample = 0);

--- xcc/misc/aud_decode.h
+++ xcc/misc/aud_decode.h
@@ -14,12 +14,12 @@
 class aud_decode  
 {
 public:
-	void init();
+	void init(int index = 0, int sample = 0);
 	void decode_chunk(const byte* audio_in, short* audio_out, int cs_chunk);
 	void encode_chunk(const short* audio_in, byte* audio_out, int cs_chunk);
 private:
-	int index;
-	int sample;
+	int m_index;
+	int m_sample;
 };
 
 void aud_decode_ws_chunk(const byte* r, char* w, int cs_chunk);
