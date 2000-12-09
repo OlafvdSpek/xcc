0914cb2e717ee0d581b3ff6f74ef353f76786529
olafvdspek <olafvdspek@108d0302-aaf7-b7c6-26e7-3e45b3cf5337>
2000-12-09 15:46:22 +0000

    Initial revision

---
+++ trunk/xcc/out_ima_adpcm_wav/config.cpp
@@ -0,0 +1,51 @@
+#include <windows.h>
+#include "config.h"
+#include "fname.h"
+#include "xcc_dirs.h"
+
+const char* app_name = "XCC IMA ADPCM WAV Writer Plug-In";
+string ini_fname;
+string out_dir;
+
+void GetPrivateProfileString(const char* name, char* value, int cb_value)
+{
+	GetPrivateProfileString(app_name, name, NULL, value, cb_value, ini_fname.c_str());
+}
+
+void WritePrivateProfileString(const char* name, const char* value)
+{
+	WritePrivateProfileString(app_name, name, value, ini_fname.c_str());
+}
+
+static void config_init()
+{
+	Cfname fname = GetModuleFileName();
+	fname.set_ext(".ini");
+	ini_fname = fname;
+}
+
+void config_read()
+{
+	config_init();
+	char b[MAX_PATH] = "";
+	GetPrivateProfileString("out_dir", b, MAX_PATH);
+	out_dir = strlen(b) ? b : xcc_dirs::get_ra2_dir();
+}
+
+void config_write()
+{
+	WritePrivateProfileString("out_dir", out_dir.c_str());
+}
+
+const char* config_out_dir()
+{
+	return out_dir.c_str();
+}
+
+void config_out_dir(const char* dir)
+{
+	out_dir = dir;
+	int l = out_dir.length();
+	if (l && out_dir[l - 1] != '\\')
+		out_dir += '\\';
+}
