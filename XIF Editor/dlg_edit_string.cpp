5de3b67a8d152df001a3ea5d651b99008110a4c5
olafvdspek <olafvdspek@108d0302-aaf7-b7c6-26e7-3e45b3cf5337>
2000-12-09 15:15:49 +0000

    Initial revision

---
+++ XIF Editor/dlg_edit_string.cpp
@@ -0,0 +1,62 @@
+// dlg_edit_string.cpp : implementation file
+//
+
+#include "stdafx.h"
+#include "xif editor.h"
+#include "dlg_edit_string.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#undef THIS_FILE
+static char THIS_FILE[] = __FILE__;
+#endif
+
+/////////////////////////////////////////////////////////////////////////////
+// Cdlg_edit_string dialog
+
+
+Cdlg_edit_string::Cdlg_edit_string(CWnd* pParent /*=NULL*/)
+	: CDialog(Cdlg_edit_string::IDD, pParent)
+{
+	//{{AFX_DATA_INIT(Cdlg_edit_string)
+	m_key = _T("");
+	m_value = _T("");
+	//}}AFX_DATA_INIT
+}
+
+
+void Cdlg_edit_string::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+	//{{AFX_DATA_MAP(Cdlg_edit_string)
+	DDX_Text(pDX, IDC_KEY, m_key);
+	DDX_Text(pDX, IDC_VALUE, m_value);
+	//}}AFX_DATA_MAP
+}
+
+
+BEGIN_MESSAGE_MAP(Cdlg_edit_string, CDialog)
+	//{{AFX_MSG_MAP(Cdlg_edit_string)
+	//}}AFX_MSG_MAP
+END_MESSAGE_MAP()
+
+/////////////////////////////////////////////////////////////////////////////
+// Cdlg_edit_string message handlers
+
+void Cdlg_edit_string::set(const string& key, const string& value)
+{
+	m_key = key.c_str();
+	m_value = value.c_str();
+}
+
+string Cdlg_edit_string::get()
+{
+	return m_value;
+}
+
+BOOL Cdlg_edit_string::OnInitDialog() 
+{
+	CDialog::OnInitDialog();
+	GotoDlgCtrl(GetDlgItem(IDC_VALUE));
+	return false;
+}
