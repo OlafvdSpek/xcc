f1a3a24ab29202053fa46626a2188c40ce62c0cf
olafvdspek <olafvdspek@108d0302-aaf7-b7c6-26e7-3e45b3cf5337>
2000-12-09 16:54:05 +0000

    added option for relative resize

--- trunk/xcc/XCC Mixer/ResizeDlg.cpp
+++ trunk/xcc/XCC Mixer/ResizeDlg.cpp
@@ -21,6 +21,9 @@ CResizeDlg::CResizeDlg(CWnd* pParent /*=NULL*/)
 	//{{AFX_DATA_INIT(CResizeDlg)
 	m_cx = 0;
 	m_cy = 0;
+	m_relative_cx = 100;
+	m_relative_cy = 100;
+	m_maintain_aspect_ratio = true;
 	//}}AFX_DATA_INIT
 }
 
@@ -30,14 +33,22 @@ void CResizeDlg::DoDataExchange(CDataExchange* pDX)
 	CDialog::DoDataExchange(pDX);
 	//{{AFX_DATA_MAP(CResizeDlg)
 	DDX_Text(pDX, IDC_CX, m_cx);
-	DDV_MinMaxInt(pDX, m_cx, 0, 4095);
+	DDV_MinMaxInt(pDX, m_cx, 0, 16384);
 	DDX_Text(pDX, IDC_CY, m_cy);
+	DDV_MinMaxInt(pDX, m_cy, 0, 16384);
+	DDX_Text(pDX, IDC_RELATIVE_CX, m_relative_cx);
+	DDX_Text(pDX, IDC_RELATIVE_CY, m_relative_cy);
+	DDX_Check(pDX, IDC_MAINTAIN_ASPECT_RATIO, m_maintain_aspect_ratio);
 	//}}AFX_DATA_MAP
 }
 
 
 BEGIN_MESSAGE_MAP(CResizeDlg, CDialog)
 	//{{AFX_MSG_MAP(CResizeDlg)
+	ON_EN_UPDATE(IDC_CX, OnUpdateCX)
+	ON_EN_UPDATE(IDC_CY, OnUpdateCY)
+	ON_EN_UPDATE(IDC_RELATIVE_CX, OnUpdateRelativeCx)
+	ON_EN_UPDATE(IDC_RELATIVE_CY, OnUpdateRelativeCy)
 	//}}AFX_MSG_MAP
 END_MESSAGE_MAP()
 
@@ -46,7 +57,64 @@ END_MESSAGE_MAP()
 
 void CResizeDlg::set_size(int cx, int cy)
 {
-	m_cx = cx;
-	m_cy = cy;
+	m_cx = m_original_cx = cx;
+	m_cy = m_original_cy = cy;
+	m_relative_cx = 100;
+	m_relative_cy = 100;
 	m_aspect_ratio = static_cast<float>(m_cx) / m_cy;
+	update_relative();
+}
+
+void CResizeDlg::OnUpdateCX() 
+{
+	if (UpdateData(true) && m_maintain_aspect_ratio)
+	{
+		m_cy = m_cx / m_aspect_ratio + .5;
+		update_relative();
+		UpdateData(false);
+	}
+}
+
+void CResizeDlg::OnUpdateCY() 
+{
+	if (UpdateData(true) && m_maintain_aspect_ratio)
+	{
+		m_cx = m_cy * m_aspect_ratio + .5;
+		update_relative();
+		UpdateData(false);
+	}
+}
+
+void CResizeDlg::OnUpdateRelativeCx() 
+{
+	if (UpdateData(true))
+	{
+		if (m_maintain_aspect_ratio)
+			m_relative_cy = m_relative_cx;
+		update_absolut();
+		UpdateData(false);
+	}
+}
+
+void CResizeDlg::OnUpdateRelativeCy() 
+{
+	if (UpdateData(true))
+	{
+		if (m_maintain_aspect_ratio)
+			m_relative_cx = m_relative_cy;
+		update_absolut();
+		UpdateData(false);
+	}
+}
+
+void CResizeDlg::update_absolut()
+{
+	m_cx = m_original_cx * m_relative_cx / 100;
+	m_cy = m_original_cy * m_relative_cy / 100;
+}
+
+void CResizeDlg::update_relative()
+{
+	m_relative_cx = m_cx * 100 / m_original_cx;
+	m_relative_cy = m_cy * 100 / m_original_cy;
 }
