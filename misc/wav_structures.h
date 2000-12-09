b1b816f7ffe5c8cfa02eea86ddd2384a4e0e96f4
olafvdspek <olafvdspek@108d0302-aaf7-b7c6-26e7-3e45b3cf5337>
2000-12-09 16:24:07 +0000

    added ima adpcm wav structs

--- misc/wav_structures.h
+++ misc/wav_structures.h
@@ -6,32 +6,61 @@
 
 struct t_wav_chunk_header
 {
-	dword id;
-	dword size;
+	int id;
+	int size;
 };
 
 struct t_wav_format_chunk
 {
 	t_wav_chunk_header header;
-	word formattag;
-	word c_channels;
-	long samplerate;
-	long byterate;
-	word blockalign;
-	word cbits_sample;
+	__int16 formattag;
+	__int16 c_channels;
+	__int32 samplerate;
+	__int32 byterate;
+	__int16 blockalign;
+	__int16 cbits_sample;
 };
 
 struct t_wav_header  
 {
 	t_wav_chunk_header file_header;
-	dword form_type;
+	int form_type;
 	t_wav_format_chunk format_chunk;
+	t_wav_chunk_header data_chunk_header;
+};
+
+struct t_wav_ima_adpcm_format_chunk
+{
+	t_wav_chunk_header header;
+	__int16 formattag;
+	__int16 c_channels;
+	__int32 samplerate;
+	__int32 byterate;
+	__int16 blockalign;
+	__int16 cbits_sample;
+	unsigned __int16 cb_extra;
+	unsigned __int16 cb_block;
+};
+
+struct t_wav_fact_chunk
+{
+	t_wav_chunk_header header;
+	int c_samples;
+};
+
+struct t_wav_ima_adpcm_header  
+{
+	t_wav_chunk_header file_header;
+	int form_type;
+	t_wav_ima_adpcm_format_chunk format_chunk;
+	t_wav_fact_chunk fact_chunk;
 	t_wav_chunk_header data_chunk_header;
 };
 
 #pragma pack(pop)
 
-const dword wav_file_id = *(dword*)"RIFF";
-const dword wav_form_id = *(dword*)"WAVE";
-const dword wav_format_id = *(dword*)"fmt ";
-const dword wav_data_id = *(dword*)"data";
+const int wav_file_id = *(int*)"RIFF";
+const int wav_form_id = *(int*)"WAVE";
+const int wav_format_id = *(int*)"fmt ";
+const int wav_fact_id = *(int*)"fact";
+const int wav_data_id = *(int*)"data";
