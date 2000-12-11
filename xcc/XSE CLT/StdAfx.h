536437554ee4811ea313768256e75d634ac7f436
olafvdspek <olafvdspek@108d0302-aaf7-b7c6-26e7-3e45b3cf5337>
2000-12-11 11:25:59 +0000

    initial revision

---
+++ xcc/XSE CLT/StdAfx.h
@@ -0,0 +1,29 @@
+// stdafx.h : include file for standard system include files,
+//  or project specific include files that are used frequently, but
+//      are changed infrequently
+//
+
+#if !defined(AFX_STDAFX_H__96134463_CF52_11D4_A95D_0050042229FC__INCLUDED_)
+#define AFX_STDAFX_H__96134463_CF52_11D4_A95D_0050042229FC__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#pragma warning(disable: 4503)
+#pragma warning(disable: 4786)
+#pragma warning(disable: 4800)
+
+#include <assert.h>
+#include <climits>
+#include <memory>
+#include <map>
+#include <set>
+#include <string>
+
+using namespace std;
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_STDAFX_H__96134463_CF52_11D4_A95D_0050042229FC__INCLUDED_)
