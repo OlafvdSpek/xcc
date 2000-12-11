65812932440e8216a7f96b0a10550662247eb4f3
olafvdspek <olafvdspek@108d0302-aaf7-b7c6-26e7-3e45b3cf5337>
2000-12-11 11:25:59 +0000

    initial revision

---
+++ trunk/xcc/XSE CLT/XSE CLT.cpp
@@ -0,0 +1,97 @@
+// XSE CLT.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include "mix_file.h"
+#include "mix_file_write.h"
+#include "wav_file.h"
+#include "xcc_dirs.h"
+#include "xse.h"
+
+using namespace std;
+
+int main(int c_args, char** arg)
+{
+	cerr << " XSE CLT, Copyright (C) Olaf van der Spek 2000                XCC@XCC.TMFWeb.NL" << endl
+		<< endl;
+	if (c_args != 2)
+	{
+		cerr << "Syntax:" << endl
+			<< "  xse_clt - or" << endl
+			<< "  xse_clt +" << endl;
+		return 1;
+	}
+	const string mode = arg[1];
+	xcc_dirs::load_from_registry();
+	Cxse xse;
+	int error = xse.open();
+	if (!error)
+	{
+		const Cxse::t_map& map = xse.map();
+		if (mode == "-")
+		{
+			Cmix_file language;
+			error = language.open(xcc_dirs::get_ra2_dir() + "language.mix");
+			if (!error)
+			{
+				language.set_game(game_ra2);
+				Cmix_file audio;
+				error = audio.open("audio.mix", language);
+				if (!error)
+				{
+					audio.set_game(game_ra2);
+					Caudio_idx_file g;
+					error = g.open("audio.idx", audio);
+					if (!error)
+					{
+						Cmix_file_write out_f;
+						for (int i = 0; i < g.c_sounds(); i++)
+						{
+							const t_audio_idx_entry& s  = g.sound_entry(i);
+							Cxse::t_map::const_iterator i = map.find(s.fname);
+							if (i == map.end() || i->second.size != s.size)
+							{
+								Cvirtual_file f;
+								error = xse.extract(s.fname, f);
+								if (!error)
+									out_f.add_file(static_cast<string>(s.fname) + ".wav", f.data(), f.size());
+								
+							}
+						}
+						if (!error)
+							error = out_f.write("audio.diff");
+						g.close();
+					}
+					audio.close();
+				}
+				language.close();
+			}
+		}
+		else if (mode == "+")
+		{
+			Cmix_file audio;
+			error = audio.open("audio.diff");
+			if (!error)
+			{
+				for (int i = 0; i < audio.get_c_files(); i++)
+				{
+					Cwav_file f;
+					int id = audio.get_id(i);
+					error = f.open(id, audio);
+					if (!error)
+					{
+						if (f.is_valid())
+							error = xse.insert(Cfname(audio.get_name(id)).get_ftitle(), f);
+						f.close();
+					}
+				}
+				if (!error)
+					xse.write_idx_file();
+				audio.close();
+			}
+		}
+		xse.close();
+	}
+	return 0;
+}
