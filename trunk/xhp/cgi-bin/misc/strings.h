b7bc4191ba0949f3c7f2130e8d9d3cf83407cae6
olafvdspek <olafvdspek@108d0302-aaf7-b7c6-26e7-3e45b3cf5337>
2000-12-20 15:42:39 +0000

    initial revision

---
+++ trunk/xhp/cgi-bin/misc/strings.h
@@ -0,0 +1,35 @@
+// strings.h: interface for the Cstrings class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#if !defined(AFX_STRINGS_H__6FC64BA0_CF65_11D4_A95D_0050042229FC__INCLUDED_)
+#define AFX_STRINGS_H__6FC64BA0_CF65_11D4_A95D_0050042229FC__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+class Cstrings  
+{
+public:
+	int add_string(string v);
+	int read(fstream& f);
+	int write(fstream& f);
+	Cstrings();
+	~Cstrings();
+
+	const char* get_string(int offset)
+	{
+		return m_data + offset;
+	}
+private:
+	char* m_data;
+	int m_size;
+};
+
+#endif // !defined(AFX_STRINGS_H__6FC64BA0_CF65_11D4_A95D_0050042229FC__INCLUDED_)
