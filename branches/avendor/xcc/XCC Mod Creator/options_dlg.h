d0def65c5e8666595328a10c80a637d1f172e97a
nobody <nobody@108d0302-aaf7-b7c6-26e7-3e45b3cf5337>
2000-12-29 11:42:03 +0000

    This commit was manufactured by cvs2svn to create branch 'avendor'.

---
+++ branches/avendor/xcc/XCC Mod Creator/options_dlg.h
@@ -0,0 +1,57 @@
+#if !defined(AFX_OPTIONS_DLG_H__9FA091C0_D599_11D4_A95D_0050042229FC__INCLUDED_)
+#define AFX_OPTIONS_DLG_H__9FA091C0_D599_11D4_A95D_0050042229FC__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+// options_dlg.h : header file
+//
+
+#include "xcc_mod.h"
+
+/////////////////////////////////////////////////////////////////////////////
+// Coptions_dlg dialog
+
+class Coptions_dlg : public CDialog
+{
+// Construction
+public:
+	Cxcc_mod::t_options get() const;
+	void set(Cxcc_mod::t_options options);
+	Coptions_dlg(CWnd* pParent = NULL);   // standard constructor
+
+// Dialog Data
+	//{{AFX_DATA(Coptions_dlg)
+	enum { IDD = IDD_OPTIONS };
+	int		m_game;
+	CString	m_link;
+	CString	m_link_title;
+	CString	m_mail;
+	CString	m_name;
+	CString	m_mod_name;
+	//}}AFX_DATA
+
+
+// Overrides
+	// ClassWizard generated virtual function overrides
+	//{{AFX_VIRTUAL(Coptions_dlg)
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+	//}}AFX_VIRTUAL
+
+// Implementation
+protected:
+
+	// Generated message map functions
+	//{{AFX_MSG(Coptions_dlg)
+	//}}AFX_MSG
+	DECLARE_MESSAGE_MAP()
+private:
+	CString m_reg_key;
+};
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_OPTIONS_DLG_H__9FA091C0_D599_11D4_A95D_0050042229FC__INCLUDED_)
---
+++ branches/avendor/xcc/XCC Mod Creator/resource.h
@@ -0,0 +1,40 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by XCC Mod Creator.rc
+//
+#define IDD_ABOUTBOX                    100
+#define CG_IDR_POPUP                    102
+#define ID_VIEW_ARRANGE                 127
+#define IDR_MAINFRAME                   128
+#define IDR_XCCMODTYPE                  129
+#define IDD_OPTIONS                     130
+#define IDC_NAME                        1005
+#define IDC_MAIL                        1006
+#define IDC_LINK_TITLE                  1007
+#define IDC_LINK                        1008
+#define IDC_MOD_NAME                    1009
+#define IDC_GAME                        1015
+#define ID_EDIT_INSERT                  32771
+#define ID_EDIT_DELETE                  32772
+#define ID_POPUP_OPEN                   32775
+#define ID_FILE_EXPORT                  32776
+#define ID_EDIT_INVERT_SELECTION        32778
+#define ID_VIEW_REFRESH                 32779
+#define ID_VIEW_OPTIONS                 32780
+#define ID_VIEW_LAUNCH                  32781
+#define ID_MOD_CLEAR_GAME_DIRECTORY     32782
+#define ID_MOD_ACTIVATE                 32783
+#define ID_MOD_DEACTIVATE               32784
+#define ID_POPUP_EXPLORE                32785
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_3D_CONTROLS                     1
+#define _APS_NEXT_RESOURCE_VALUE        132
+#define _APS_NEXT_COMMAND_VALUE         32786
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           103
+#endif
+#endif
