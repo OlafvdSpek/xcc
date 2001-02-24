46381d0cce4b885896795c195ef2639dc194da53
olafvdspek <olafvdspek@108d0302-aaf7-b7c6-26e7-3e45b3cf5337>
2001-02-24 01:07:54 +0000

    initial import

---
+++ xcc/XCC RA2 Map Updater/resource.h
@@ -0,0 +1,22 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by XCC RA2 Map Updater.rc
+//
+#define IDD_XCCRA2MAPUPDATER_DIALOG     102
+#define IDR_MAINFRAME                   128
+#define IDD_DOWNLOAD_UPDATE             129
+#define IDC_SOURCE                      1009
+#define IDC_DESTINATION                 1010
+#define IDC_PROGRESS                    1011
+#define IDC_FSIZE                       1012
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1013
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
---
+++ xcc/XCC RA2 Radar Customizer/resource.h
@@ -0,0 +1,25 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by XCC RA2 Radar Customizer.rc
+//
+#define IDD_XCCRA2RADARCUSTOMIZER_DIALOG 102
+#define IDR_MAINFRAME                   128
+#define IDC_RED                         1001
+#define IDC_GREEN                       1002
+#define IDC_BLUE                        1003
+#define IDC_SNOW                        1004
+#define IDC_TEMPERATE                   1005
+#define IDC_RESET                       1006
+#define IDC_URBAN                       1007
+#define IDC_RELATIVE                    1008
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1005
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
