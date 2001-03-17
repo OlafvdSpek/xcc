6c6da37f01efe91345d28aaa8e0553f3c199c35c
olafvdspek <olafvdspek@108d0302-aaf7-b7c6-26e7-3e45b3cf5337>
2001-03-17 16:11:22 +0000

    initial import

---
+++ trunk/xcc/3DS2VXL FE/3DS2VXL FE.cpp
@@ -0,0 +1,61 @@
+// 3DS2VXL FE.cpp : Defines the class behaviors for the application.
+//
+
+#include "stdafx.h"
+#include "3DS2VXL FE.h"
+#include "3DS2VXL FEDlg.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#undef THIS_FILE
+static char THIS_FILE[] = __FILE__;
+#endif
+
+/////////////////////////////////////////////////////////////////////////////
+// CMy3DS2VXLFEApp
+
+BEGIN_MESSAGE_MAP(CMy3DS2VXLFEApp, CWinApp)
+	//{{AFX_MSG_MAP(CMy3DS2VXLFEApp)
+	//}}AFX_MSG
+	ON_COMMAND(ID_HELP, CWinApp::OnHelp)
+END_MESSAGE_MAP()
+
+/////////////////////////////////////////////////////////////////////////////
+// CMy3DS2VXLFEApp construction
+
+CMy3DS2VXLFEApp::CMy3DS2VXLFEApp()
+{
+}
+
+/////////////////////////////////////////////////////////////////////////////
+// The one and only CMy3DS2VXLFEApp object
+
+CMy3DS2VXLFEApp theApp;
+
+/////////////////////////////////////////////////////////////////////////////
+// CMy3DS2VXLFEApp initialization
+
+BOOL CMy3DS2VXLFEApp::InitInstance()
+{
+	// Standard initialization
+
+#ifdef _AFXDLL
+	Enable3dControls();			// Call this when using MFC in a shared DLL
+#else
+	Enable3dControlsStatic();	// Call this when linking to MFC statically
+#endif
+
+	CMy3DS2VXLFEDlg dlg;
+	m_pMainWnd = &dlg;
+	int nResponse = dlg.DoModal();
+	if (nResponse == IDOK)
+	{
+	}
+	else if (nResponse == IDCANCEL)
+	{
+	}
+
+	// Since the dialog has been closed, return FALSE so that we exit the
+	//  application, rather than start the application's message pump.
+	return FALSE;
+}
---
+++ trunk/xcc/3DS2VXL FE/resource.h
@@ -0,0 +1,32 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by 3DS2VXL FE.rc
+//
+#define IDD_MY3DS2VXLFE_DIALOG          102
+#define IDR_MAINFRAME                   128
+#define IDC_MAX_SIZE					1000
+#define IDC_SOURCE                      1001
+#define IDC_SOURCE_BROWSE               1002
+#define IDC_SKYBOX                      1003
+#define IDC_SKYBOX_BROWSE               1004
+#define IDC_FLIP_X                      1005
+#define IDC_FLIP_Y                      1006
+#define IDC_SWAP_XY                     1007
+#define IDC_ESTIMATE_ORIGIN             1008
+#define IDC_SGN                         1009
+#define IDC_SGAL                        1010
+#define IDC_TEXTURES                    1011
+#define IDC_FLIP_X2                     1012
+#define IDC_DESTINATION                 1014
+#define IDC_DESTINATION_BROWSE          1015
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1014
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
