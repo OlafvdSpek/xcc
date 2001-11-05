2b71c1c94338986d690f34c99d189f73ae79d3fb
olafvdspek <olafvdspek@108d0302-aaf7-b7c6-26e7-3e45b3cf5337>
2001-11-05 10:16:02 +0000

    no message

--- trunk/misc/multi_line.h
+++ trunk/misc/multi_line.h
@@ -20,6 +20,7 @@ public:
 	Cmulti_line(const char* is);
 	Cmulti_line(const string &is);
 	int get_next_int(const char separator = ',');
+	int get_next_int(const string& separator);
 	string get_next_line(const char separator = ',', bool remove_ws = true);
 	string get_next_line(const string& separator, bool remove_ws = true);
 
