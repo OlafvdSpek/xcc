48c630e3164ea253965e7804d8a8adb676533254
olafvdspek <olafvdspek@108d0302-aaf7-b7c6-26e7-3e45b3cf5337>
2002-04-29 17:20:29 +0000

    initial revision

---
+++ xcc/XCC MIX CLT/StdAfx.h
@@ -0,0 +1,22 @@
+// stdafx.h : include file for standard system include files,
+//  or project specific include files that are used frequently, but
+//      are changed infrequently
+//
+
+#if !defined(AFX_STDAFX_H__76D16868_9BF6_45D1_900C_E213719A2D1F__INCLUDED_)
+#define AFX_STDAFX_H__76D16868_9BF6_45D1_900C_E213719A2D1F__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#pragma warning(disable: 4786)
+
+#include <cassert>
+#include <iostream>
+#include <map>
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_STDAFX_H__76D16868_9BF6_45D1_900C_E213719A2D1F__INCLUDED_)
