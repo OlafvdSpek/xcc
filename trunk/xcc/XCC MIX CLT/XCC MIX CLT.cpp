e8f06f3f3372014892424eaa4b1bdb97f2228f07
olafvdspek <olafvdspek@108d0302-aaf7-b7c6-26e7-3e45b3cf5337>
2002-04-29 17:20:29 +0000

    initial revision

---
+++ trunk/xcc/XCC MIX CLT/XCC MIX CLT.cpp
@@ -0,0 +1,28 @@
+#include "stdafx.h"
+#include "mix_file.h"
+
+int main(int argc, char* argv[])
+{
+	cerr << " XCC MIX CLT, Copyright (C) Olaf van der Spek 2002            XCC@XCC.TMFWeb.NL" << endl
+		<< endl;
+	if (argc < 2)
+	{
+		cerr << "Syntax:" << endl
+			<< "  xcc_mix_clt <mixfile> <file>" << endl;
+		return 1;
+	}
+	Cmix_file mix;
+	if (mix.open(argv[1]))
+		return cerr << "Unable to open " << argv[1] << endl, 1;
+	int error = 0;
+	for (int i = 2; i < argc; i++)
+	{
+		Cvirtual_binary d = mix.get_vdata(argv[i]);
+		if (!d.data())
+			cerr << "Unable to open " << argv[i] << endl, error = 1;
+		else if (d.export(argv[i]))
+			cerr << "Unable to save " << argv[i] << endl, error = 1;
+	}
+	mix.close();
+	return error;
+}
