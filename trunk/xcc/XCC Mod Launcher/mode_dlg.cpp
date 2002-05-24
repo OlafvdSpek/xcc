15b5a991e125abd86935d020967beb5e164e4e97
olafvdspek <olafvdspek@108d0302-aaf7-b7c6-26e7-3e45b3cf5337>
2002-05-24 15:31:57 +0000

    initial revision

---
+++ trunk/xcc/XCC Mod Launcher/mode_dlg.cpp
@@ -0,0 +1,73 @@
+// mode_dlg.cpp : implementation file
+//
+
+#include "stdafx.h"
+#include "XCC Mod Launcher.h"
+#include "mode_dlg.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#undef THIS_FILE
+static char THIS_FILE[] = __FILE__;
+#endif
+
+/////////////////////////////////////////////////////////////////////////////
+// Cmode_dlg dialog
+
+
+Cmode_dlg::Cmode_dlg(CWnd* pParent /*=NULL*/)
+	: CDialog(Cmode_dlg::IDD, pParent)
+{
+	//{{AFX_DATA_INIT(Cmode_dlg)
+		// NOTE: the ClassWizard will add member initialization here
+	//}}AFX_DATA_INIT
+}
+
+
+void Cmode_dlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+	//{{AFX_DATA_MAP(Cmode_dlg)
+	DDX_Control(pDX, IDC_MODE, m_modes);
+	//}}AFX_DATA_MAP
+}
+
+
+BEGIN_MESSAGE_MAP(Cmode_dlg, CDialog)
+	//{{AFX_MSG_MAP(Cmode_dlg)
+	//}}AFX_MSG_MAP
+END_MESSAGE_MAP()
+
+/////////////////////////////////////////////////////////////////////////////
+// Cmode_dlg message handlers
+
+BOOL Cmode_dlg::OnInitDialog() 
+{
+	CDialog::OnInitDialog();
+	{
+		typedef Cxcc_mod::t_mode_map t_map;
+		const t_map& map = mod().mode_map();
+		for (t_map::const_iterator i = map.begin(); i != map.end(); i++)
+		{
+			if (i->first != -1)
+				m_modes.AddString(i->second.c_str());
+		}
+		m_modes.SetCurSel(0);
+	}
+	return true;
+}
+
+void Cmode_dlg::OnOK() 
+{
+	if (!UpdateData())
+		return;
+	CString mode, module;
+	m_modes.GetWindowText(mode);
+	m_mode = mode;
+	CDialog::OnOK();
+}
+
+void Cmode_dlg::mod(const Cxcc_mod& mod)
+{
+	m_mod = &mod;
+}
---
+++ trunk/xcc/XCC Mod Launcher/mode_dlg.h
@@ -0,0 +1,63 @@
+#if !defined(AFX_MODE_DLG_H__EF893481_23A9_11D6_B861_0000B4936994__INCLUDED_)
+#define AFX_MODE_DLG_H__EF893481_23A9_11D6_B861_0000B4936994__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+// mode_dlg.h : header file
+//
+
+#include "xcc_mod.h"
+
+/////////////////////////////////////////////////////////////////////////////
+// Cmode_dlg dialog
+
+class Cmode_dlg : public CDialog
+{
+// Construction
+public:
+	void mod(const Cxcc_mod& mod);
+	Cmode_dlg(CWnd* pParent = NULL);   // standard constructor
+
+	const Cxcc_mod& mod() const
+	{
+		return *m_mod;
+	}
+	
+	string mode() const
+	{
+		return m_mode;
+	}
+// Dialog Data
+	//{{AFX_DATA(Cmode_dlg)
+	enum { IDD = IDD_MODE };
+	CComboBox	m_modes;
+	//}}AFX_DATA
+
+
+// Overrides
+	// ClassWizard generated virtual function overrides
+	//{{AFX_VIRTUAL(Cmode_dlg)
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+	//}}AFX_VIRTUAL
+
+// Implementation
+protected:
+
+	// Generated message map functions
+	//{{AFX_MSG(Cmode_dlg)
+	virtual BOOL OnInitDialog();
+	virtual void OnOK();
+	//}}AFX_MSG
+	DECLARE_MESSAGE_MAP()
+private:
+	const Cxcc_mod* m_mod;
+	string m_mode;
+};
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_MODE_DLG_H__EF893481_23A9_11D6_B861_0000B4936994__INCLUDED_)
