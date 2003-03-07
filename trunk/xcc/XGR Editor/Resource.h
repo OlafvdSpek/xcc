e58c2e04c9849b2d0dadd3c042275283676fb14d
olafvdspek <olafvdspek@108d0302-aaf7-b7c6-26e7-3e45b3cf5337>
2003-03-07 19:19:53 +0000

    initial revision

---
+++ trunk/xcc/XGR Editor/Resource.h
@@ -0,0 +1,21 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by XGR Editor.rc
+//
+#define CG_IDR_POPUP_LEFT_VIEW          102
+#define ID_VIEW_ARRANGE                 127
+#define IDR_MAINFRAME                   128
+#define IDR_XGREDITYPE                  129
+#define ID_POPUP_DELETE                 32771
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_3D_CONTROLS                     1
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32772
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           103
+#endif
+#endif
