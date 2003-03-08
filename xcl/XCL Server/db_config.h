8164b8b5696c66a91f9dba92f37618559d4bd9e0
olafvdspek <olafvdspek@108d0302-aaf7-b7c6-26e7-3e45b3cf5337>
2003-03-08 18:32:38 +0000

    initial import

---
+++ xcl/XCL Server/db_config.h
@@ -0,0 +1,17 @@
+// db_config.h: interface for the Cdb_config class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#if !defined(AFX_DB_CONFIG_H__87CD83FD_6AB2_4283_B347_A3D077495483__INCLUDED_)
+#define AFX_DB_CONFIG_H__87CD83FD_6AB2_4283_B347_A3D077495483__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+extern const char* mysql_host;
+extern const char* mysql_user;
+extern const char* mysql_pass;
+extern const char* mysql_db;
+
+#endif // !defined(AFX_DB_CONFIG_H__87CD83FD_6AB2_4283_B347_A3D077495483__INCLUDED_)
