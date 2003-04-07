0bcb17b04d70f9f055618875b5258ae1e09377e1
olafvdspek <olafvdspek@108d0302-aaf7-b7c6-26e7-3e45b3cf5337>
2003-04-07 13:32:46 +0000

    no message

---
+++ xcc/misc/wol/ra2_names.h
@@ -0,0 +1,15 @@
+#if !defined(AFX_RA2_NAMES_H__00ADDA5E_BC0C_406B_B88F_7ED2C6750266__INCLUDED_)
+#define AFX_RA2_NAMES_H__00ADDA5E_BC0C_406B_B88F_7ED2C6750266__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#include <string>
+
+using namespace std;
+
+string get_country_name(int i);
+string get_map_name(const string& v);
+
+#endif // !defined(AFX_RA2_NAMES_H__00ADDA5E_BC0C_406B_B88F_7ED2C6750266__INCLUDED_)
