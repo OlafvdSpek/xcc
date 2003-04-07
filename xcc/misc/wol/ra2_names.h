812b8955bcd07ea584c0f8513372d4ea4e9048f0
olafvdspek <olafvdspek@108d0302-aaf7-b7c6-26e7-3e45b3cf5337>
2003-04-07 14:02:31 +0000

    no message

--- xcc/misc/wol/ra2_names.h
+++ xcc/misc/wol/ra2_names.h
@@ -11,5 +11,9 @@ using namespace std;
 
 string get_country_name(int i);
 string get_map_name(const string& v);
+string get_ra2_building_name(int v);
+string get_ra2_infantry_name(int v);
+string get_ra2_plane_name(int v);
+string get_ra2_vehicle_name(int v);
 
 #endif // !defined(AFX_RA2_NAMES_H__00ADDA5E_BC0C_406B_B88F_7ED2C6750266__INCLUDED_)
