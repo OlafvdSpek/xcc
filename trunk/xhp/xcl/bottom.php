809ef5767d1e151edd87d985fae37a39ce690ee7
olafvdspek <olafvdspek@108d0302-aaf7-b7c6-26e7-3e45b3cf5337>
2004-03-01 16:39:43 +0000

    no message

---
+++ trunk/xhp/xcl/bottom.php
@@ -0,0 +1,22 @@
+<hr>
+<table width="100%">
+	<tr>
+		<td valign=top>
+			<a href="http://communityteam.de/">Community Team</a> | <a href="http://strike-team.net/">Strike Team</a> | <a href="http://xccu.sourceforge.net/">XCC Home Page</a>
+		<td align=center valign=top>
+			<p>
+				<a href="https://www.paypal.com/xclick/business=st_xcl%40hotmail.com;item_name=XCL+Fund;image_url=http%3A//xccu.sourceforge.net/button.gif;no_shipping=1;no_note=1;cs=1;tax=0;currency_code=GBP"><img src="https://www.paypal.com/images/x-click-but04.gif" alt="PayPal Donate"></a>
+			<p>
+				<script language="JavaScript" type="text/javascript" src="http://m1.nedstatbasic.net/basic.js">
+				</script>
+				<script language="JavaScript" type="text/javascript">
+				<!--
+					nedstatbasic("ACYaRA5mKkSNw9DcBlC+vYnGi83A", 0);
+				// -->
+				</script>
+				<noscript>
+					<a target="_blank" href="http://v1.nedstatbasic.net/stats?ACYaRA5mKkSNw9DcBlC+vYnGi83A"><img src="http://m1.nedstatbasic.net/n?id=ACYTXwDHntQULdYZIrOjZRmDVgjQ" alt="NedStat Statistics"></a>
+				</noscript>
+		<td align=right valign=top>
+			<?php echo(gmdate("H:i d-m-Y")) ?>
+</table>
