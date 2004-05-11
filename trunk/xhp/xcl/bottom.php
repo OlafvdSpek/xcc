d1e670884c31d1ef0a9307e1c0968413203d7e9b
olafvdspek <olafvdspek@108d0302-aaf7-b7c6-26e7-3e45b3cf5337>
2004-05-11 19:21:28 +0000

    no message

--- trunk/xhp/xcl/bottom.php
+++ trunk/xhp/xcl/bottom.php
@@ -2,10 +2,15 @@
 <table width="100%">
 	<tr>
 		<td valign=top>
-			<a href="http://communityteam.de/">Community Team</a> | <a href="http://strike-team.net/">Strike Team</a> | <a href="http://xccu.sourceforge.net/">XCC Home Page</a>
-		<td align=center valign=top>
-			<p>
-				<a href="http://communityteam.de/"><img src="http://communityteam.de/images/banner/ct_banner.gif"></a>
+			<a href="http://champ-site.com/">Champ Site</a> |
+			<a href="http://communityteam.de/">Community Team</a> |
+			<a href="http://dune2k.com/">FED2k</a> |
+			<a href="http://strike-team.net/">Strike Team</a> |
+			<a href="http://xccu.sourceforge.net/">XCC Home Page</a>
+		<td align=right valign=top>
+			<?php echo(gmdate("H:i d-m-Y")) ?>
+	<tr>
+		<td align=center colspan=3>
 			<p>
 				<a href="https://www.paypal.com/xclick/business=st_xcl%40hotmail.com&amp;item_name=XCL+Fund&amp;image_url=http%3A//xccu.sourceforge.net/button.gif&amp;no_shipping=1&amp;no_note=1&amp;cs=1&amp;tax=0&amp;currency_code=GBP"><img src="https://www.paypal.com/images/x-click-but04.gif" alt="PayPal Donate"></a>
 			<p>
@@ -19,6 +24,6 @@
 				<noscript>
 					<a target="_blank" href="http://v1.nedstatbasic.net/stats?ACYaRA5mKkSNw9DcBlC+vYnGi83A"><img src="http://m1.nedstatbasic.net/n?id=ACYTXwDHntQULdYZIrOjZRmDVgjQ" alt="NedStat Statistics"></a>
 				</noscript>
-		<td align=right valign=top>
-			<?php echo(gmdate("H:i d-m-Y")) ?>
+			<p>
+				<a href="http://communityteam.de/"><img src="http://communityteam.de/images/banner/ct_banner.gif" alt="Community Team"></a>
 </table>
