551f8663cd5377609ae0a23dd59181ba79faa953
olafvdspek <olafvdspek@108d0302-aaf7-b7c6-26e7-3e45b3cf5337>
2004-07-03 19:06:39 +0000

    no message

--- trunk/xcc/XCC Game Spy Player/ChildFrm.cpp
+++ trunk/xcc/XCC Game Spy Player/ChildFrm.cpp
@@ -2,7 +2,6 @@
 //
 
 #include "stdafx.h"
-#include "XCC Game Spy Player.h"
 
 #include "ChildFrm.h"
 
--- trunk/xcc/XCC Game Spy Player/dlg_open.cpp
+++ trunk/xcc/XCC Game Spy Player/dlg_open.cpp
@@ -2,7 +2,6 @@
 //
 
 #include "stdafx.h"
-#include "XCC Game Spy Player.h"
 #include "dlg_open.h"
 
 #include "wol/ra2_names.h"
