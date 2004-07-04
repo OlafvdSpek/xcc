e37eed854b434cb72cacf5d5f1cba852bc5e4e00
olafvdspek <olafvdspek@108d0302-aaf7-b7c6-26e7-3e45b3cf5337>
2004-07-04 12:12:14 +0000

    no message

--- trunk/misc/wav_header.h
+++ trunk/misc/wav_header.h
@@ -11,6 +11,6 @@
 #pragma once
 #endif // _MSC_VER >= 1000
 
-void wav_fill_header(t_wav_header &header, long samplesize, long samplerate, long cbits_sample, long c_channels);
+void wav_fill_header(t_wav_header& header, int samplesize, int samplerate, int cbits_sample, int c_channels);
 
 #endif // !defined(AFX_WAV_HEADER_H__7BAA68C3_CBCF_11D2_99E5_AC7708C14908__INCLUDED_)
