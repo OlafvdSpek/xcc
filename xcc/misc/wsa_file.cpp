d0194dbaa197da00592d7cafbd8a243b2e360068
olafvdspek <olafvdspek@108d0302-aaf7-b7c6-26e7-3e45b3cf5337>
2004-07-10 12:37:30 +0000

    no message

--- xcc/misc/wsa_file.cpp
+++ xcc/misc/wsa_file.cpp
@@ -5,10 +5,35 @@
 #include "shp_decode.h"
 #include "string_conversion.h"
 
+void Cwsa_file::decode(void* d) const
+{
+	memset(d, 0, cb_image());
+	Cvirtual_binary s;
+	byte* w = reinterpret_cast<byte*>(d);
+	for (int i = 0; i < cf(); i++)
+	{
+		if (i)
+			memcpy(w, w - cb_image(), cb_image());
+		if (get_offset(i))
+		{
+			decode80(get_frame(i), s.write_start(64 << 10));
+			decode40(s, w);
+		}
+		w += cb_image();
+	}
+}
+
+Cvirtual_image Cwsa_file::vimage() const
+{
+	Cvirtual_binary image;
+	decode(image.write_start(cb_video()));
+	return Cvirtual_image(image, cx(), cf() * cy(), cb_pixel(), palet(), true);
+}
+
 int Cwsa_file::extract_as_pcx(const Cfname& name, t_file_type ft) const
 {
 	t_palet palet;
-	memcpy(palet, get_palet(), sizeof(t_palet));
+	memcpy(palet, Cwsa_file::palet(), sizeof(t_palet));
 	convert_palet_18_to_24(palet);
 	Cvirtual_binary frame;
 	Cvirtual_binary s;
