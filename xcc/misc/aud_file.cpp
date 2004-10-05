adc422d1a786024068caec76c7459c707084f530
olafvdspek <olafvdspek@108d0302-aaf7-b7c6-26e7-3e45b3cf5337>
2004-10-05 19:03:47 +0000

    no message

--- xcc/misc/aud_file.cpp
+++ xcc/misc/aud_file.cpp
@@ -84,49 +84,30 @@ Cvirtual_binary Caud_file::decode()
 
 int Caud_file::extract_as_wav(const string& name)
 {
-	int error = 0;
+	Cvirtual_binary d = decode();
+	if (!d)
+		return 1;
 	Cfile32 f;
-	error = f.open(name, GENERIC_WRITE);
-	if (!error)
-	{
-		int cb_sample = get_cb_sample();
-		int cs_remaining = get_c_samples();	
-		t_wav_header header;
-		memset(&header, 0, sizeof(t_wav_header));
-		header.file_header.id = wav_file_id;
-		header.file_header.size = sizeof(header) - sizeof(header.file_header) + (cs_remaining << 1);
-		header.form_type = wav_form_id;
-		header.format_chunk.header.id = wav_format_id;
-		header.format_chunk.header.size = sizeof(header.format_chunk) - sizeof(header.format_chunk.header);
-		header.format_chunk.formattag = 1;
-		header.format_chunk.c_channels = 1;
-		header.format_chunk.samplerate = get_samplerate();
-		header.format_chunk.byterate =  cb_sample * get_samplerate();
-		header.format_chunk.blockalign = cb_sample;
-		header.format_chunk.cbits_sample = cb_sample << 3;
-		header.data_chunk_header.id = wav_data_id;
-		header.data_chunk_header.size = cb_sample * cs_remaining;
-		error = f.write(&header, sizeof(t_wav_header));
-		if (!error)
-		{			
-			int chunk_i = 0;
-			aud_decode decode;
-			decode.init();
-			const byte* s;
-			short d[1024];
-			while (cs_remaining)
-			{				
-				const t_aud_chunk_header& header = *get_chunk_header(chunk_i);
-				int cs_chunk = header.size_out / cb_sample;
-				s = get_chunk_data(chunk_i++);
-				decode.decode_chunk(s, d, cs_chunk);
-				error = f.write(d, cb_sample * cs_chunk);
-				cs_remaining -= cs_chunk;
-				if (error)
-					break;
-			}			
-		}
-		f.close();
-	}
-	return error;
+	int error = f.open(name, GENERIC_WRITE);
+	if (error)
+		return error;
+	int cb_sample = get_cb_sample();
+	int cs_remaining = get_c_samples();	
+	t_wav_header header;
+	memset(&header, 0, sizeof(t_wav_header));
+	header.file_header.id = wav_file_id;
+	header.file_header.size = sizeof(header) - sizeof(header.file_header) + (cs_remaining << 1);
+	header.form_type = wav_form_id;
+	header.format_chunk.header.id = wav_format_id;
+	header.format_chunk.header.size = sizeof(header.format_chunk) - sizeof(header.format_chunk.header);
+	header.format_chunk.formattag = 1;
+	header.format_chunk.c_channels = 1;
+	header.format_chunk.samplerate = get_samplerate();
+	header.format_chunk.byterate =  cb_sample * get_samplerate();
+	header.format_chunk.blockalign = cb_sample;
+	header.format_chunk.cbits_sample = cb_sample << 3;
+	header.data_chunk_header.id = wav_data_id;
+	header.data_chunk_header.size = cb_sample * cs_remaining;
+	error = f.write(&header, sizeof(t_wav_header));
+	return error ? error : f.write(d, d.size());
 }
