5d26a42bfaebc5d04425642efc25b7ccee10ef18
olafvdspek <olafvdspek@108d0302-aaf7-b7c6-26e7-3e45b3cf5337>
2004-10-05 21:49:46 +0000

    no message

--- trunk/xcc/XCC Mixer/resource.h
+++ trunk/xcc/XCC Mixer/resource.h
@@ -1280,7 +1280,7 @@
 #define _APS_3D_CONTROLS                     1
 #define _APS_NEXT_RESOURCE_VALUE        141
 #define _APS_NEXT_COMMAND_VALUE         33131
-#define _APS_NEXT_CONTROL_VALUE         1037
+#define _APS_NEXT_CONTROL_VALUE         1038
 #define _APS_NEXT_SYMED_VALUE           103
 #endif
 #endif
