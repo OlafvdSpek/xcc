84b90380bcaf10796e1b93c0b234314a2429cda8
olafvdspek <olafvdspek@108d0302-aaf7-b7c6-26e7-3e45b3cf5337>
2004-10-14 12:44:49 +0000

    no message

--- xcc/XCC Editor/list_ctrl_extension.cpp
+++ xcc/XCC Editor/list_ctrl_extension.cpp
@@ -15,11 +15,6 @@ static char THIS_FILE[] = __FILE__;
 
 Clist_ctrl_extension::Clist_ctrl_extension()
 {
-	m_max_column_i = -1;
-}
-
-Clist_ctrl_extension::~Clist_ctrl_extension()
-{
 }
 
 
@@ -37,56 +32,33 @@ void Clist_ctrl_extension::set_fixed_width_font()
 		SetFont(&m_font);
 }
 
-void Clist_ctrl_extension::add_column(const string& text, long index, dword format)
+void Clist_ctrl_extension::add_column(const string& text, int index, int format)
 {
-	LV_COLUMN column_data;
-	column_data.mask = LVCF_FMT | LVCF_SUBITEM | LVCF_TEXT;
-	column_data.fmt = format;
-	char s[256];
-	strcpy(s, text.c_str()); 
-	column_data.pszText = s;
-	column_data.iSubItem = index;
-	InsertColumn(index, &column_data);
-	if (index > m_max_column_i)
-		m_max_column_i = index;
+	InsertColumn(index, text.c_str(), format);
 }
 
-void Clist_ctrl_extension::add_item(const string& text, dword index, dword user_parameter)
+void Clist_ctrl_extension::add_item(const string& text, int index, int user_parameter)
 {
-	LV_ITEM item_data;
-	item_data.mask = LVIF_TEXT | LVIF_PARAM;
-	item_data.iItem = index;
-	item_data.iSubItem = 0;
-	char s[256];
-	strcpy(s, text.c_str()); 
-	item_data.pszText = s;
-	item_data.lParam = user_parameter;
-	InsertItem(&item_data);
+	SetItemData(InsertItem(index, text.c_str()), user_parameter);
 }
 
-void Clist_ctrl_extension::set_item(const string& text, dword index, dword subindex)
+void Clist_ctrl_extension::set_item(const string& text, int index, int subindex)
 {
 	SetItemText(index, subindex, text.c_str());
 }
 
 void Clist_ctrl_extension::autosize_columns()
 {
-	for (long i = 0; i <= m_max_column_i; i++)
+	for (int i = 0; i < GetHeaderCtrl()->GetItemCount(); i++)
 		SetColumnWidth(i, LVSCW_AUTOSIZE);
 }
 
 void Clist_ctrl_extension::set_full_row_selection()
 {
-	ListView_SetExtendedListViewStyle(m_hWnd, ListView_GetExtendedListViewStyle(m_hWnd) | LVS_EX_FULLROWSELECT);
+	SetExtendedStyle(GetExtendedStyle() | LVS_EX_FULLROWSELECT);
 }
 
-long Clist_ctrl_extension::get_focused_item() const
+int Clist_ctrl_extension::get_focused_item() const
 {
 	return GetNextItem(-1, LVNI_ALL | LVNI_FOCUSED);
-}
-
-BOOL Clist_ctrl_extension::PreCreateWindow(CREATESTRUCT& cs) 
-{
-	// cs.dwExStyle |= LVS_EX_FULLROWSELECT;
-	return CListCtrl::PreCreateWindow(cs);
 }
