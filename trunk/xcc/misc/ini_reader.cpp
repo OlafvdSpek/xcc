27cc43d0e246fd65dfa74f5e71bb8ddf70bf225f
olafvdspek <olafvdspek@108d0302-aaf7-b7c6-26e7-3e45b3cf5337>
2004-10-15 20:40:04 +0000

    no message

--- trunk/xcc/misc/ini_reader.cpp
+++ trunk/xcc/misc/ini_reader.cpp
@@ -24,11 +24,6 @@ int Cini_reader::find_id(string s, const char* t[], int count)
 	return i;
 }
 
-static bool is_section_start(const string &s)
-{
-	return s[0] == '[' && s[s.length() - 1] == ']';
-}
-
 static int split_key_now(const string& key, string& name, string& value)
 {
 	int i = key.find('=');
@@ -168,31 +163,6 @@ int Cini_reader::process_line(string line)
 		i++;
 	}
 	return 0;
-	int comment_start = line.find(';');
-	if (comment_start != string::npos)
-		line = line.substr(0, comment_start);
-	trim(line);
-	if (line.empty())
-		return 0;
-	if (is_section_start(line))
-	{
-		if (m_section_open)
-			process_section_end();
-		m_section_open = true;
-		line = line.substr(1, line.length() - 2);
-		if (m_lower_case)
-			return process_section_start(to_lower(line));
-		return process_section_start(line);
-	}
-	if (!process_section())
-		return 0;
-	string name;
-	string value;
-	if (split_key_now(line, name, value))
-		return !m_skip_errors;
-	if (m_lower_case)
-		return process_key(to_lower(name), value);
-	return process_key(name, value);
 }
 
 void Cini_reader::process_section_end()
