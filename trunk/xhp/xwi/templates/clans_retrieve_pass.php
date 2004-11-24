682ed5d8f06c9f91c189ec2a173acc435b466e92
olafvdspek <olafvdspek@108d0302-aaf7-b7c6-26e7-3e45b3cf5337>
2004-11-24 18:20:14 +0000

    no message

--- trunk/xhp/xwi/templates/clans_retrieve_pass.php
+++ trunk/xhp/xwi/templates/clans_retrieve_pass.php
@@ -1,5 +1,5 @@
 <table>
-	<form action="?a=create" method=post>
+	<form action="?a=retrieve_pass" method=post>
 		<tr>
 			<td align=right>Clan name:
 			<td><input type=text name=cname maxlength=6 value="<?php echo(htmlspecialchars($cname))?>">
