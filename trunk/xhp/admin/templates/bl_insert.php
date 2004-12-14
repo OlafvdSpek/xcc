5e4af09c7499f734d393eb94a9312986b0b1275b
olafvdspek <olafvdspek@108d0302-aaf7-b7c6-26e7-3e45b3cf5337>
2004-12-14 19:49:22 +0000

    no message

---
+++ trunk/xhp/admin/templates/bl_insert.php
@@ -0,0 +1,11 @@
+<table>
+	<form method=get>
+		<input type=hidden name=a value="bl_insert_submit">
+		<input type=hidden name=pid value="<?php printf("%d", $pid) ?>">
+		<tr><td align=right>Name:<td><?php echo $name ?>
+		<tr><td align=right>Reason:<td><input type=text name=reason size=60 value="<?php echo htmlspecialchars($reason) ?>">
+		<tr><td align=right>Link:<td><input type=text name=link size=60 value="<?php echo htmlspecialchars($link) ?>">
+		<tr><td align=right>Duration:<td><input type=text name=dura size=60 value="<?php printf("%d", $dura) ?>"> days
+		<tr><td><td><input type=submit>
+	</form>
+</table>
