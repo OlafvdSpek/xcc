84e02975d213cba61d320ed291250ab7d911979f
olafvdspek <olafvdspek@108d0302-aaf7-b7c6-26e7-3e45b3cf5337>
2004-12-14 21:46:09 +0000

    no message

---
+++ xhp/admin/templates/edit_warning_insert_ipa.php
@@ -0,0 +1,7 @@
+<table>
+	<form action="?a=edit_warning_insert_ipa_submit" method=post>
+		<input type=hidden name=wid value=<?=$wid?>>
+		<tr><th>IPA<td><input type=text name=ipa>
+		<tr><td><td><input type=submit value="Add IPA"></tr>
+	</form>
+</table>
