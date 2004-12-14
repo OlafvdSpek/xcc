3b9af3376361704e85ea268507c0a1f33e7f84e4
olafvdspek <olafvdspek@108d0302-aaf7-b7c6-26e7-3e45b3cf5337>
2004-12-14 19:49:22 +0000

    no message

---
+++ xhp/admin/templates/links.php
@@ -0,0 +1,7 @@
+<a href="logins.php">Logins</a> |
+<a href="players.php?">Players</a> |
+<a href="players.php?a=bad_passes">Bad passes</a> |
+<a href="players.php?a=invalid_serials">Invalid serials</a> |
+<a href="players.php?a=xbl">Show BL</a> |
+<a href="players.php?a=motds">Show MOTDs</a> |
+<a href="players.php?a=xwsvs">XWSVS</a>
