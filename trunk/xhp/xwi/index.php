4617712d2c652b7d484e86b810674976fe5d2e0f
olafvdspek <olafvdspek@108d0302-aaf7-b7c6-26e7-3e45b3cf5337>
2006-01-25 19:21:05 +0000

    no message

--- trunk/xhp/xwi/index.php
+++ trunk/xhp/xwi/index.php
@@ -1,13 +1,18 @@
-<?php
-	ob_start('ob_gzhandler');
-?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <link rel=stylesheet href="/egx.css">
 <script type="text/javascript" src="/xcl/xcl.js"></script>
 <title>XCC Clans</title>
 <table width="100%"><tr><td valign=bottom><p class=page_title>XCC Clans<td align=right valign=bottom><a href="/xwi/">Clans</a> | <a href="http://xccu.sourceforge.net/cgi-bin/forum.cgi">Forum</a> | <a href="http://xwis.net:4005/">Online</a> | <a href="http://strike-team.net/nuke/html/modules.php?op=modload&amp;name=News&amp;file=article&amp;sid=13">Rules</a> | <a href="http://xccu.sourceforge.net/utilities/XGS.zip" title="XCC Game Spy">XGS</a> | <a href="/downloads/XWISB.zip" title="XCC WOL IRC Server Beeper">XWISB</a> | <a href="/downloads/XWISC.exe" title="XCC WOL IRC Server Client">XWISC</a><br><a href="/xcl/?hof=" title="Hall of Fame">HoF</a> | <a href="/xcl/?hos=" title="Hall of Shame">HoS</a> | <a href="/xcl/?">Home</a> | <a href="/xcl/?stats=">Stats</a></table>
 <hr>
-<a href="?a=create">Create</a> | <a href="?">Home</a> | <a href="?a=invite">Invite</a> | <a href="?a=join">Join</a> | <a href="?a=kick">Kick</a> | <a href="?a=leave">Leave</a> | <a href="?a=reset_pass">Reset pass</a> | <a href="?a=delete_nick">Delete nick</a>
+<a href="?a=create">Create</a> |
+<a href="?">Home</a> |
+<a href="?a=invite">Invite</a> |
+<a href="?a=join">Join</a> |
+<a href="?a=kick">Kick</a> |
+<a href="?a=leave">Leave</a> |
+<a href="?a=reset_pass">Reset pass</a> |
+<a href="?a=delete_nick">Delete nick</a> |
+<a href="http://strike-team.net/forums/index.php?showforum=88">Hosted Clan Forums</a>
 <hr>
 <?php
 	function apgar_encode($v)
@@ -400,7 +405,15 @@
 	}
 ?>
 <hr>
-<a href="?a=create">Create</a> | <a href="?">Home</a> | <a href="?a=invite">Invite</a> | <a href="?a=join">Join</a> | <a href="?a=kick">Kick</a> | <a href="?a=leave">Leave</a> | <a href="?a=reset_pass">Reset pass</a> | <a href="?a=delete_nick">Delete nick</a>
+<a href="?a=create">Create</a> |
+<a href="?">Home</a> |
+<a href="?a=invite">Invite</a> |
+<a href="?a=join">Join</a> |
+<a href="?a=kick">Kick</a> |
+<a href="?a=leave">Leave</a> |
+<a href="?a=reset_pass">Reset pass</a> |
+<a href="?a=delete_nick">Delete nick</a> |
+<a href="http://strike-team.net/forums/index.php?showforum=88">Hosted Clan Forums</a>
 <?php
 	echo('<script type="text/javascript">');
 	printf("page_bottom(%d);", time());
