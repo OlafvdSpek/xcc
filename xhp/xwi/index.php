1faa37cde05d016a36f2b3bb82c5dadd4cb4245e
olafvdspek <olafvdspek@108d0302-aaf7-b7c6-26e7-3e45b3cf5337>
2006-01-25 19:55:42 +0000

    no message

--- xhp/xwi/index.php
+++ xhp/xwi/index.php
@@ -136,31 +136,36 @@
 					echo("Invalid clan abbreviation");
 				else if (!valid_clan_name($cname))
 					echo("Invalid clan name");
-				else if ($player = get_player2($name, $pass))
+				else if (get_player($name))
 				{
-					if ($clan = mysql_fetch_array(db_query(sprintf("select name, full_name from xwi_clans where name = '%s' or full_name = '%s'", addslashes($cabbrev), addslashes($cname)))))
-						printf("Clan %s (%s) already exists", $clan['name'], $clan['full_name']);
-					else
+					if ($player = get_player2($name, $pass))
 					{
-						do
+						if ($clan = mysql_fetch_array(db_query(sprintf("select name, full_name from xwi_clans where name = '%s' or full_name = '%s'", addslashes($cabbrev), addslashes($cname)))))
+							printf("Clan %s (%s) already exists", $clan['name'], $clan['full_name']);
+						else
 						{
-							$cpass = new_security_code();
-							$results = db_query(sprintf("select count(*) from xwi_clans where pass = md5('%s')", $cpass));
-							$result = mysql_fetch_array($results);
+							do
+							{
+								$cpass = new_security_code();
+								$results = db_query(sprintf("select count(*) from xwi_clans where pass = md5('%s')", $cpass));
+								$result = mysql_fetch_array($results);
+							}
+							while ($result['0']);
+							db_query(sprintf("insert into xwi_clans (name, full_name, pass, icq, mail, msn, site, mtime, ctime) values (lcase('%s'), '%s', md5('%s'), %d, lcase('%s'), lcase('%s'), lcase('%s'), unix_timestamp(), unix_timestamp())", addslashes($cabbrev), addslashes($cname), $cpass, $icq, addslashes($mail), addslashes($msn), addslashes($site)));
+							$cid = mysql_insert_id();
+							db_query(sprintf("update xwi_players set cid = %d where pid = %d", $cid, $player['pid']));
+							$clan = get_clan($cid);
+							printf("Player %s created clan %s<br>", $player['name'], $clan['name']);
+							printf("The clan admin pass is %s", $cpass);
+							if (strlen($mail))
+								mail($mail, sprintf("XWI Clan Manager: Clan %s created", $clan['name']), sprintf("Player %s created clan %s with admin pass %s from IP address %s", $player['name'], $clan['name'], $cpass, $_SERVER['REMOTE_ADDR']), "from: XWIS <xwis>");
 						}
-						while ($result['0']);
-						db_query(sprintf("insert into xwi_clans (name, full_name, pass, icq, mail, msn, site, mtime, ctime) values (lcase('%s'), '%s', md5('%s'), %d, lcase('%s'), lcase('%s'), lcase('%s'), unix_timestamp(), unix_timestamp())", addslashes($cabbrev), addslashes($cname), $cpass, $icq, addslashes($mail), addslashes($msn), addslashes($site)));
-						$cid = mysql_insert_id();
-						db_query(sprintf("update xwi_players set cid = %d where pid = %d", $cid, $player['pid']));
-						$clan = get_clan($cid);
-						printf("Player %s created clan %s<br>", $player['name'], $clan['name']);
-						printf("The clan admin pass is %s", $cpass);
-						if (strlen($mail))
-							mail($mail, sprintf("XWI Clan Manager: Clan %s created", $clan['name']), sprintf("Player %s created clan %s with admin pass %s from IP address %s", $player['name'], $clan['name'], $cpass, $_SERVER['REMOTE_ADDR']), "from: XWIS <xwis>");
 					}
+					else
+						printf("Invalid pass for player %s", htmlspecialchars($name));
 				}
 				else
-					echo("Wrong name/pass combo");
+					printf("Player %s not found", htmlspecialchars($name));
 				echo("<hr>");
 			}
 			require('templates/clans_create.php');
@@ -170,27 +175,32 @@
 		{
 			if ($name || $pass)
 			{
-				if ($player = get_player2($name, $pass))
+				if (get_player($name))
 				{
-					$results = db_query(sprintf("select count(*) from xcl_players where lid & 1 and name = '%s'", $name));
-					$result = mysql_fetch_array($results);
-					if ($result['0'])
-						printf("Player %s is already in ladder", $player['name']);
-					else
+					if ($player = get_player2($name, $pass))
 					{
-						$results = db_query(sprintf("select to_days(now()) - to_days(ctime) from xwi_players where pid = %d", $player['pid']));
+						$results = db_query(sprintf("select count(*) from xcl_players where lid & 1 and name = '%s'", $name));
 						$result = mysql_fetch_array($results);
-						if ($result['0'] < 32)
-							printf("Only players that were created more than 32 days ago can be deleted. Player %s was created %d days ago", $player['name'], $result['0']);
+						if ($result['0'])
+							printf("Player %s is already in ladder", $player['name']);
 						else
 						{
-							db_query(sprintf("update xwi_players set flags = flags | 2 where pid = %d", $player['pid']));
-							printf("Player %s has been deleted", $player['name']);
+							$results = db_query(sprintf("select to_days(now()) - to_days(ctime) from xwi_players where pid = %d", $player['pid']));
+							$result = mysql_fetch_array($results);
+							if ($result['0'] < 32)
+								printf("Only players that were created more than 32 days ago can be deleted. Player %s was created %d days ago", $player['name'], $result['0']);
+							else
+							{
+								db_query(sprintf("update xwi_players set flags = flags | 2 where pid = %d", $player['pid']));
+								printf("Player %s has been deleted", $player['name']);
+							}
 						}
 					}
+					else
+						printf("Invalid pass for player %s", htmlspecialchars($name));
 				}
 				else
-					echo("Wrong name/pass combo");
+					printf("Player %s not found", htmlspecialchars($name));
 				echo("<hr>");
 			}
 			require('templates/players_delete.php');
@@ -236,31 +246,36 @@
 			$cpass = trim($_POST['cpass']);
 			if ($name || $pass || $cpass)
 			{
-				if ($player = get_player2($name, $pass))
+				if (get_player($name))
 				{
-					db_query("delete from xwi_clan_invites where unix_timestamp(now()) - unix_timestamp(ctime) > 3 * 24 * 60 * 60");
-					if ($player['cid'])
+					if ($player = get_player2($name, $pass))
 					{
-						$clan = get_clan($player['cid']);
-						printf("Player %s is already in clan %s", $player['name'], $clan['name']);
-					}
-					else if ($clan_invite = mysql_fetch_array(db_query(sprintf("select * from xwi_clan_invites where pass = md5('%s')", addslashes($cpass)))))
-					{
-						if ($clan_invite['pid'] == $player['pid'])
+						db_query("delete from xwi_clan_invites where unix_timestamp(now()) - unix_timestamp(ctime) > 3 * 24 * 60 * 60");
+						if ($player['cid'])
 						{
-							db_query(sprintf("delete from xwi_clan_invites where pid = %d and cid = %d", $player['pid'], $clan_invite['cid']));
-							db_query(sprintf("update xwi_players set cid = %d where pid = %d", $clan_invite['cid'], $player['pid']));
-							$clan = get_clan($clan_invite['cid']);
-							printf("Player %s joined clan %s<br>", $player['name'], $clan['name']);
+							$clan = get_clan($player['cid']);
+							printf("Player %s is already in clan %s", $player['name'], $clan['name']);
+						}
+						else if ($clan_invite = mysql_fetch_array(db_query(sprintf("select * from xwi_clan_invites where pass = md5('%s')", addslashes($cpass)))))
+						{
+							if ($clan_invite['pid'] == $player['pid'])
+							{
+								db_query(sprintf("delete from xwi_clan_invites where pid = %d and cid = %d", $player['pid'], $clan_invite['cid']));
+								db_query(sprintf("update xwi_players set cid = %d where pid = %d", $clan_invite['cid'], $player['pid']));
+								$clan = get_clan($clan_invite['cid']);
+								printf("Player %s joined clan %s<br>", $player['name'], $clan['name']);
+							}
+							else
+								echo("Wrong invitation");
 						}
 						else
-							echo("Wrong invitation");
+							echo("Wrong clan pass");
 					}
 					else
-						echo("Wrong clan pass");
+						printf("Invalid pass for player %s", htmlspecialchars($name));
 				}
 				else
-					echo("Wrong name/pass combo");
+					printf("Player %s not found", htmlspecialchars($name));
 				echo("<hr>");
 			}
 			require('templates/clans_join.php');
@@ -346,22 +361,27 @@
 		{
 			if ($name || $pass)
 			{
-				if ($player = get_player2($name, $pass))
+				if (get_player($name))
 				{
-					if ($player['cid'])
+					if ($player = get_player2($name, $pass))
 					{
-						db_query(sprintf("update xwi_players set cid = 0 where pid = %d", $player['pid']));
-						$clan = get_clan($player['cid']);
-						printf("Player %s left clan %s", $player['name'], $clan['name']);
-						$result = mysql_fetch_array(db_query(sprintf("select count(*) from xwi_players where cid = %d", $player['cid'])));
-						if (!$result['0'])
-							db_query(sprintf("delete from xwi_clans where cid = %d", $player['cid']));
+						if ($player['cid'])
+						{
+							db_query(sprintf("update xwi_players set cid = 0 where pid = %d", $player['pid']));
+							$clan = get_clan($player['cid']);
+							printf("Player %s left clan %s", $player['name'], $clan['name']);
+							$result = mysql_fetch_array(db_query(sprintf("select count(*) from xwi_players where cid = %d", $player['cid'])));
+							if (!$result['0'])
+								db_query(sprintf("delete from xwi_clans where cid = %d", $player['cid']));
+						}
+						else
+							printf("Player %s is not in a clan", $player['name']);
 					}
 					else
-						printf("Player %s is not in a clan", $player['name']);
+						printf("Invalid pass for player %s", htmlspecialchars($name));
 				}
 				else
-					echo("Wrong name/pass combo");
+					printf("Player %s not found", htmlspecialchars($name));
 				echo("<hr>");
 			}
 			require('templates/clans_leave.php');
