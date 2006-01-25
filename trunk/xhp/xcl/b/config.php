fb61eba7faa4c882b9455ca716e29b3b8b0b6a0f
olafvdspek <olafvdspek@108d0302-aaf7-b7c6-26e7-3e45b3cf5337>
2006-01-25 21:20:22 +0000

    no message

---
+++ trunk/xhp/xcl/b/config.php
@@ -0,0 +1,18 @@
+<?php
+	$frozen = false;
+	$tables = array
+	(
+		'games' => 'xcl_games',
+		'games_players' => 'xcl_games_players',
+		'hof' => 'xcl_hof',
+		'maps' => 'xcl_maps',
+		'players' => 'xcl_players',
+		'stats_afps' => 'xcl_stats_afps',
+		'stats_countries' => 'xcl_stats_countries',
+		'stats_dura' => 'xcl_stats_dura',
+		'stats_games' => 'xcl_stats_games',
+		'stats_gsku' => 'xcl_stats_gsku',
+		'stats_maps' => 'xcl_stats_maps',
+		'stats_time' => 'xcl_stats_time',
+	);
+?>
---
+++ trunk/xhp/xcl/b/config_prev.php
@@ -0,0 +1,18 @@
+<?php
+	$frozen = true;
+	$tables = array
+	(
+		'games' => 'xcl_prev_games',
+		'games_players' => 'xcl_prev_games_players',
+		'hof' => 'xcl_hof',
+		'maps' => 'xcl_maps',
+		'players' => 'xcl_prev_players',
+		'stats_afps' => 'xcl_prev_stats_afps',
+		'stats_countries' => 'xcl_prev_stats_countries',
+		'stats_dura' => 'xcl_prev_stats_dura',
+		'stats_games' => 'xcl_prev_stats_games',
+		'stats_gsku' => 'xcl_prev_stats_gsku',
+		'stats_maps' => 'xcl_prev_stats_maps',
+		'stats_time' => 'xcl_prev_stats_time',
+	);
+?>
