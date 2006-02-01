73b23f99479386070d20704b87ec513d2ccf8d77
olafvdspek <olafvdspek@108d0302-aaf7-b7c6-26e7-3e45b3cf5337>
2006-02-01 15:15:34 +0000

    no message

--- trunk/xhp/admin/cron.php
+++ trunk/xhp/admin/cron.php
@@ -1,9 +1,13 @@
 <?php
+	chdir('/home/olaf/xwis.net/php/admin');
 	require_once('../xcc_common.php');
 
 	db_connect();
+	db_query("delete xwi_clans from xwi_clans left join xwi_players p using (cid) where p.cid is null");
+	db_query("delete from xwi_clan_reset_pass_requests where ctime < unix_timestamp() - 7 * 24 * 60 * 60");
 	db_query("delete from bl");
 	db_query("insert into bl select distinct name from xwi_players inner join xwi_serials using (sid) where wtime > now()");
-	db_query("update bl inner join xcl_players using (name) set points = 0");
-	db_query("update xwi_serials s inner join xwi_serials_new sn on s.serial = md5(sn.serial) set valid = 0 where valid = -1");
+	// db_query("insert ignore into bl select distinct p.name from xbl inner join xwi_logins l using (sid) inner join xwi_players p using (pid)");
+	// select distinct p1.name from xbl inner join xwi_players p0 using (name) inner join xwi_logins using (pid) inner join xwi_players p1 using (sid);
+	db_query("update xwi_serials s inner join xwi_serials_new sn on s.serial = md5(sn.serial) set valid = 0 where mtime < unix_timestamp() - 60 * 60 and valid = -1");
 ?>
