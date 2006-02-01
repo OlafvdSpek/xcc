ad229e23fa3187ea87dcf8465c535844f0750f89
olafvdspek <olafvdspek@108d0302-aaf7-b7c6-26e7-3e45b3cf5337>
2006-02-01 13:06:20 +0000

    no message

--- trunk/xhp/xwi/index.php
+++ trunk/xhp/xwi/index.php
@@ -1,34 +1,3 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
-<link rel=stylesheet href="/egx.css">
-<script type="text/javascript" src="/xcl/xcl.js"></script>
-<title>XCC Clans</title>
-<table width="100%">
-	<tr>
-		<td valign=bottom><p class=page_title>XCC Clans
-		<td align=right valign=bottom>
-			<a href="/xcl/">Clans</a> |
-			<a href="http://strike-team.net/forums/">Forum</a> |
-			<a href="/xcl/?">Online</a> |
-			<a href="http://strike-team.net/nuke/html/modules.php?op=modload&amp;name=News&amp;file=article&amp;sid=13">Rules</a> |
-			<a href="http://xccu.sourceforge.net/utilities/XGS.exe" title="XCC Game Spy">XGS</a> |
-			<a href="/downloads/XWISC.exe" title="XCC WOL IRC Server Client">XWISC</a><br>
-			<a href="/xcl/?hof=" title="Hall of Fame">HoF</a> |
-			<a href="/xcl/?hos=" title="Hall of Shame">HoS</a> |
-			<a href="/xcl/">Home</a> |
-			<a href="/xcl/?recent_games=">Recent</a> |
-			<a href="/xcl/?stats=">Stats</a>
-</table>
-<hr>
-<a href="?a=create">Create</a> |
-<a href="?">Home</a> |
-<a href="?a=invite">Invite</a> |
-<a href="?a=join">Join</a> |
-<a href="?a=kick">Kick</a> |
-<a href="?a=leave">Leave</a> |
-<a href="?a=reset_pass">Reset pass</a> |
-<a href="?a=delete_nick">Delete nick</a> |
-<a href="http://strike-team.net/forums/index.php?showforum=88">Hosted Clan Forums</a>
-<hr>
 <?php
 	function apgar_encode($v)
 	{
@@ -91,6 +60,7 @@
 		return true;
 	}
 
+	include('templates/top.php');
 	require("../xcc_common.php");
 
 	db_connect();
@@ -438,19 +408,5 @@
 			echo("</table>");
 		}
 	}
-?>
-<hr>
-<a href="?a=create">Create</a> |
-<a href="?">Home</a> |
-<a href="?a=invite">Invite</a> |
-<a href="?a=join">Join</a> |
-<a href="?a=kick">Kick</a> |
-<a href="?a=leave">Leave</a> |
-<a href="?a=reset_pass">Reset pass</a> |
-<a href="?a=delete_nick">Delete nick</a> |
-<a href="http://strike-team.net/forums/index.php?showforum=88">Hosted Clan Forums</a>
-<?php
-	echo('<script type="text/javascript">');
-	printf("page_bottom(%d);", time());
-	echo('</script>');
+	include('templates/bottom.php');
 ?>
