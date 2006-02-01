c602cf575547ccc80cf296dae0fa5b08b29457fb
olafvdspek <olafvdspek@108d0302-aaf7-b7c6-26e7-3e45b3cf5337>
2006-02-01 13:06:20 +0000

    no message

---
+++ xhp/xwi/templates/bottom.php
@@ -0,0 +1,15 @@
+<hr>
+<a href="?a=create">Create</a> |
+<a href="?">Home</a> |
+<a href="?a=invite">Invite</a> |
+<a href="?a=join">Join</a> |
+<a href="?a=kick">Kick</a> |
+<a href="?a=leave">Leave</a> |
+<a href="?a=reset_pass">Reset pass</a> |
+<a href="?a=delete_nick">Delete nick</a> |
+<a href="http://strike-team.net/forums/index.php?showforum=88">Hosted Clan Forums</a>
+<?php
+	echo('<script type="text/javascript">');
+	printf("page_bottom(%d);", time());
+	echo('</script>');
+?>
