519fc7497bad7570835bd98f578f528af64b3524
olafvdspek <olafvdspek@108d0302-aaf7-b7c6-26e7-3e45b3cf5337>
2006-02-05 23:17:43 +0000

    no message

--- trunk/xhp/admin/templates/bl_insert.php
+++ trunk/xhp/admin/templates/bl_insert.php
@@ -6,6 +6,6 @@
 		<tr><td align=right>Reason:<td><input type=text name=reason size=60 value="<?php echo htmlspecialchars($reason) ?>">
 		<tr><td align=right>Link:<td><input type=text name=link size=60 value="<?php echo htmlspecialchars($link) ?>">
 		<tr><td align=right>Duration:<td><input type=text name=dura size=60 value="<?php printf("%d", $dura) ?>"> days
-		<tr><td><td><input type=submit>
+		<tr><td><td><input type=submit></tr>
 	</form>
 </table>
