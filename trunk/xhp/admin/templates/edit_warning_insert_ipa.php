8ce209e551fdccc982e7424aee52fce176862d7d
olafvdspek <olafvdspek@108d0302-aaf7-b7c6-26e7-3e45b3cf5337>
2006-02-05 22:12:18 +0000

    no message

--- trunk/xhp/admin/templates/edit_warning_insert_ipa.php
+++ trunk/xhp/admin/templates/edit_warning_insert_ipa.php
@@ -1,7 +1,6 @@
 <table>
 	<form action="?a=edit_warning_insert_ipa_submit" method=post>
 		<input type=hidden name=wid value=<?=$wid?>>
-		<tr><th>IPA<td><input type=text name=ipa>
-		<tr><td><td><input type=submit value="Add IPA"></tr>
+		<tr><th>IPA<td><input type=text name=ipa><td><input type=submit value="Add IPA"></tr>
 	</form>
 </table>
