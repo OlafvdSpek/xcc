59a5be92aa2153cac12175f7c56569eac62abbff
olafvdspek <olafvdspek@108d0302-aaf7-b7c6-26e7-3e45b3cf5337>
2006-02-07 13:38:28 +0000

    no message

---
+++ trunk/xhp/admin/update_hof.php
@@ -0,0 +1,35 @@
+<?php
+	require_once('common.php');
+
+	function update_hof($date, $lid)
+	{
+		$rows = db_query(sprintf("select name from xcl_prev_players where lid = %d order by points desc, pid limit 10", $lid));
+		$rank = 1;
+		while ($row = mysql_fetch_assoc($rows))
+			db_query(sprintf("insert into xcl_hof (date, lid, rank, name) values ('%s', %d, %d, '%s')", $date, $lid, $rank++, $row['name']));
+	}
+
+	$year = gmdate('Y');
+	$month = gmdate('m') - 1;
+	if (!$month)
+	{
+		$month += 12;
+		$year--;
+	}
+	$date = sprintf('%04d%02d00', $year, $month);
+	$row = db_query_first(sprintf("select count(*) c from xcl_hof where date = '%s'", $date));
+	if ($row['c'])
+	{
+		if ($_GET['force'] != 1)
+			die('hof exists already');
+		db_query(sprintf("delete from xcl_hof where date = '%s'", $date));
+	}
+	update_hof($date, 1);
+	update_hof($date, 2);
+	update_hof($date, 3);
+	update_hof($date, 4);
+	update_hof($date, 7);
+	update_hof($date, 8);
+	db_query("insert ignore into xcl_high_scores (name, points, rank) select name, points, rank from xcl_prev_players where lid = 3 and points");
+	db_query("update xcl_high_scores h inner join xcl_prev_players p using (name) set h.points = greatest(h.points, p.points), h.rank = least(h.rank, p.rank) where lid = 3");
+?>
